69b1013922ba98c8cca27745bb84a14a0e9cb744
Danila <belakdanila9@gmail.com>
2025-02-22 01:02:49 +1000

    Установка и настройка плагинов для сборки, и повторная сборка проекта

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,25 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html", // Указываем путь к вашему HTML-шаблону
+        }),
+        new MiniCssExtractPlugin({
+            filename: "output.css", // Имя выходного CSS-файла
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, "src/favicon.ico"), // Копируем favicon.ico
+                    to: path.resolve(__dirname, "dist"),
+                },
+            ],
+        }),
+    ],
     devServer: {
         static: {
             directory: path.join(__dirname, "src"),
@@ -12,8 +31,13 @@ module.exports = {
     entry: "./src/script.js",
     module: {
         rules: [
-            { test: /\.svg$/, use: "svg-inline-loader" },
-            { test: /\.css$/, use: ["style-loader", "css-loader"] },
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader, // Извлекает CSS в отдельный файл
+                    "css-loader", // Обрабатывает CSS
+                ],
+            },
         ],
     },
     mode: process.env.NODE_ENV === "production" ? "production" : "development",
